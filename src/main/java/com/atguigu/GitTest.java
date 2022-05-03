29fda001efd146499294a108c332fa8c3726c14e
Leon <leon1347@163.com>
2022-05-03 15:09:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author pluto
+ * @date 2022/5/3 - 15:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("get test");
+    }
+}
