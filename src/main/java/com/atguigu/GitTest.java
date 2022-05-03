ed864657a1143c56cb5ba3b84cf6b0553efbe555
Leon <leon1347@163.com>
2022-05-03 15:22:48 +0800

    hot-fix test

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("get test2");
         System.out.println("get test3");
         System.out.println("get test4");
+        System.out.println("hot-fix test");
     }
 }
